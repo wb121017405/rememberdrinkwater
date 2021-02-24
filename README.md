502d2fd4205f3a670f740be5b3b02693f043fce2
巧克力冰激凌 <121017405@qq.com>
2021-02-24 11:10:29 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,7 @@
+# rememberdrinkwater README
+
+这只是一个计时器，每小时弹出一次提醒。
+
+>在打开vscode之后沉迷代码只能用内部提示提醒自己喝水
+
+**Enjoy! 记得喝水**
---
+++ src/extension.ts
@@ -0,0 +1,43 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "rememberdrinkwater" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let drinkWaterInterval:any = null;
+	let disposable = vscode.commands.registerCommand('rememberdrinkwater.startWork', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		if (drinkWaterInterval) {
+			vscode.window.showInformationMessage('喝水小助手已经在运行了');
+			return;
+		}
+		drinkWaterInterval = setInterval(()=>{
+			vscode.window.showInformationMessage('您好,我是您的“提醒喝水小助手”希望此刻看到消息的人可以和我一起来喝一杯水。\n及时排便洗手，记得关门。\n一小时后的我继续提醒您喝水,和我一起成为一天八杯水的人吧!');
+		},3600000);
+	});
+	let disposable2 = vscode.commands.registerCommand('rememberdrinkwater.stopWork', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		clearInterval(drinkWaterInterval);
+		drinkWaterInterval = null;
+		vscode.window.showInformationMessage('喝水小助手关闭');
+	});
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
